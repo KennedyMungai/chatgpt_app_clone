d18af56402ec4b05ab33fca7d6556e39a8027790
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-23 15:35:04 +0300

    Created the login page and gave it  a modal presentation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,6 +41,7 @@ function RootLayoutNav() {
 	return (
 		<Stack>
 			<Stack.Screen name='index' options={{ headerShown: false }} />
+			<Stack.Screen name='login' options={{ presentation: 'modal' }} />
 		</Stack>
 	)
 }
