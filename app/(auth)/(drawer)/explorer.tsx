5cabb5106a3dc844f8dd88a7120abb13fe635e90
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 14:21:06 +0300

    Created the explorer component

---
+++ app/(auth)/(drawer)/explorer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Explorer = () => {
+	return (
+		<View>
+			<Text>Explorer</Text>
+		</View>
+	)
+}
+
+export default Explorer
+
+const styles = StyleSheet.create({})
