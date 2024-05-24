49724b18635404dad0fa1324340491df7741ff8b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 12:07:40 +0300

    Made some changes for the auth group

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const AuthPage = () => {
+	return (
+		<View>
+			<Text>AuthPage</Text>
+		</View>
+	)
+}
+
+export default AuthPage
+
+const styles = StyleSheet.create({})
