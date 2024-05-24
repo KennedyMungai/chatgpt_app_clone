eefd6b611a447c2ad5a74bdbd02fb75ebce8982d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 14:22:02 +0300

    Created the dalle component

---
+++ app/(auth)/(drawer)/dalle.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Dalle = () => {
+	return (
+		<View>
+			<Text>Dalle</Text>
+		</View>
+	)
+}
+
+export default Dalle
+
+const styles = StyleSheet.create({})
