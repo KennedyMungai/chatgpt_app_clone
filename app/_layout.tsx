532720473c45c4fd34558b69126484231947886b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 12:52:48 +0300

    Made some updates to the root layout file for the application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,41 @@
 import { ClerkProvider, useAuth } from '@clerk/clerk-expo'
 import { Ionicons } from '@expo/vector-icons'
-import FontAwesome from '@expo/vector-icons/FontAwesome'
 import { useFonts } from 'expo-font'
-import { Stack, router, useRouter, useSegments } from 'expo-router'
+import { SplashScreen, Stack, useRouter, useSegments } from 'expo-router'
 import * as SecureStore from 'expo-secure-store'
-import * as SplashScreen from 'expo-splash-screen'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { ActivityIndicator, TouchableOpacity, View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import 'react-native-reanimated'
 
-const CLERK_PUBLISHABLE_KEY = process.env
-	.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string
-
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+// Cache the Clerk JWT
 const tokenCache = {
 	async getToken(key: string) {
 		try {
 			return SecureStore.getItemAsync(key)
-		} catch (error) {
+		} catch (err) {
 			return null
 		}
 	},
 	async saveToken(key: string, value: string) {
 		try {
 			return SecureStore.setItemAsync(key, value)
-		} catch (error) {
+		} catch (err) {
 			return
 		}
 	}
 }
 
-export {
-	// Catch any errors thrown by the Layout component.
-	ErrorBoundary
-} from 'expo-router'
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
-export default function RootLayout() {
+const InitialLayout = () => {
 	const [loaded, error] = useFonts({
-		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-		...FontAwesome.font
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
 	})
+	const { isLoaded, isSignedIn } = useAuth()
+	const segments = useSegments()
+	const router = useRouter()
 
 	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
 	useEffect(() => {
@@ -55,68 +48,50 @@ export default function RootLayout() {
 		}
 	}, [loaded])
 
-	if (!loaded) {
-		return (
-			<View
-				style={{
-					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
-			>
-				<ActivityIndicator size={40} color='black' />
-			</View>
-		)
-	}
-
-	return <RootLayoutNav />
-}
-
-const InitialLayout = () => {
-	const { isLoaded, isSignedIn } = useAuth()
-	const segments = useSegments()
-
-	if (!isLoaded)
-		return (
-			<View
-				style={{
-					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
-			>
-				<ActivityIndicator size={40} color='black' />
-			</View>
-		)
-
 	useEffect(() => {
 		if (!isLoaded) return
 
 		const inAuthGroup = segments[0] === '(auth)'
 
 		if (isSignedIn && !inAuthGroup) {
-			// Pushing the user into the authenticated group
-			router.replace('/(auth)')
-		} else if (!isSignedIn && inAuthGroup) {
-			// Pushing the user out of the authenticated group
+			router.replace('/(auth)/')
+		} else if (!isSignedIn) {
 			router.replace('/')
 		}
 	}, [isSignedIn])
 
+	if (!loaded || !isLoaded) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+				<ActivityIndicator size='large' color='black' />
+			</View>
+		)
+	}
+
 	return (
 		<Stack>
-			<Stack.Screen name='index' options={{ headerShown: false }} />
+			<Stack.Screen
+				name='index'
+				options={{
+					headerShown: false
+				}}
+			/>
 			<Stack.Screen
 				name='login'
 				options={{
 					presentation: 'modal',
+					title: '',
 					animation: 'slide_from_bottom',
 					animationDuration: 200,
-					title: '',
-					headerStyle: {
-						backgroundColor: 'white'
+					headerTitleStyle: {
+						fontFamily: 'mon-sb'
 					},
-					headerShadowVisible: false,
 					headerLeft: () => (
 						<TouchableOpacity onPress={() => router.back()}>
 							<Ionicons name='close-outline' size={28} />
@@ -130,8 +105,6 @@ const InitialLayout = () => {
 }
 
 const RootLayoutNav = () => {
-	const router = useRouter()
-
 	return (
 		<ClerkProvider
 			publishableKey={CLERK_PUBLISHABLE_KEY!}
@@ -143,3 +116,5 @@ const RootLayoutNav = () => {
 		</ClerkProvider>
 	)
 }
+
+export default RootLayoutNav
