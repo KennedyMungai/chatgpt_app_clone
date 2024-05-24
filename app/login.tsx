4ff91c072bb901604584a423e691d846437253a0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 10:43:26 +0300

    Added some base logic for the login modal

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,86 @@
+import Colors from '@/constants/Colors'
+import { defaultStyles } from '@/constants/Styles'
 import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {
+	ActivityIndicator,
+	Image,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View
+} from 'react-native'
 
 const LoginPage = () => {
 	const { type } = useLocalSearchParams<{ type: string }>()
+	const [loading, setLoading] = useState(false)
+	const [emailAddress, setEmailAddress] = useState('')
+	const [password, setPassword] = useState('')
 
-	console.log(type)
+	const onSignUpPress = async () => {}
+
+	const onSignInPress = async () => {}
 
 	return (
-		<View style={styles.container}>
-			<Text>Login</Text>
-		</View>
+		<KeyboardAvoidingView
+			style={styles.container}
+			behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+			keyboardVerticalOffset={1}
+		>
+			{loading && (
+				<View
+					style={{
+						flex: 1,
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+				>
+					<ActivityIndicator size={40} color={'black'} />
+				</View>
+			)}
+			<Image
+				source={require('../assets/images/logo-dark.png')}
+				style={styles.logo}
+			/>
+			<Text style={styles.title}>
+				{type === 'login' ? 'Welcome Back' : 'Create your account'}
+			</Text>
+			<View style={{ marginBottom: 30 }}>
+				<TextInput
+					autoCapitalize='none'
+					placeholder='Email'
+					style={styles.inputField}
+					value={emailAddress}
+					onChangeText={setEmailAddress}
+				/>
+				<TextInput
+					placeholder='Password'
+					style={styles.inputField}
+					value={password}
+					onChangeText={setPassword}
+					autoCapitalize='none'
+					secureTextEntry
+				/>
+			</View>
+			{type === 'login' ? (
+				<TouchableOpacity
+					style={[defaultStyles.btn, styles.btnPrimary]}
+					onPress={onSignInPress}
+				>
+					<Text style={styles.btnPrimaryText}>Login</Text>
+				</TouchableOpacity>
+			) : (
+				<TouchableOpacity
+					style={[defaultStyles.btn, styles.btnPrimary]}
+					onPress={onSignUpPress}
+				>
+					<Text style={styles.btnPrimaryText}>Create an Account</Text>
+				</TouchableOpacity>
+			)}
+		</KeyboardAvoidingView>
 	)
 }
 
@@ -19,6 +89,37 @@ export default LoginPage
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: 'white',
-		flex: 1
+		flex: 1,
+		padding: 20
+	},
+	logo: {
+		width: 60,
+		height: 60,
+		alignSelf: 'center',
+		marginVertical: 80
+	},
+	title: {
+		fontSize: 30,
+		marginBottom: 20,
+		fontWeight: 'bold',
+		alignSelf: 'center'
+	},
+	inputField: {
+		marginVertical: 4,
+		height: 50,
+		borderWidth: 1,
+		borderColor: Colors.primary,
+		borderRadius: 12,
+		padding: 14,
+		backgroundColor: 'white',
+		fontSize: 20
+	},
+	btnPrimary: {
+		backgroundColor: Colors.primary,
+		marginVertical: 4
+	},
+	btnPrimaryText: {
+		color: 'white',
+		fontSize: 16
 	}
 })
