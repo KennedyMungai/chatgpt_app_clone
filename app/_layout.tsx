02aab68faea2f8d528c5f1025f8b29e55d68dded
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-24 11:12:14 +0300

    implemented clerk email authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,33 @@
+import { ClerkProvider } from '@clerk/clerk-expo'
 import { Ionicons } from '@expo/vector-icons'
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 import { useFonts } from 'expo-font'
-import { Stack, useRouter } from 'expo-router'
+import { Stack, router, useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { TouchableOpacity } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import 'react-native-reanimated'
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key)
+		} catch (error) {
+			return null
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value)
+		} catch (error) {
+			return
+		}
+	}
+}
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -39,9 +61,7 @@ export default function RootLayout() {
 	return <RootLayoutNav />
 }
 
-function RootLayoutNav() {
-	const router = useRouter()
-
+const InitialLayout = () => {
 	return (
 		<Stack>
 			<Stack.Screen name='index' options={{ headerShown: false }} />
@@ -64,5 +84,20 @@ function RootLayoutNav() {
 				}}
 			/>
 		</Stack>
+	)
+}
+
+const RootLayoutNav = () => {
+	const router = useRouter()
+
+	return (
+		<ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY!}
+			tokenCache={tokenCache}
+		>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<InitialLayout />
+			</GestureHandlerRootView>
+		</ClerkProvider>
 	)
 }
