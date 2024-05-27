1a18ca882447ef10bfb107c2230742522e1f79f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-27 10:18:12 +0300

    Created the header-dropdown component

---
+++ components/header-dropdown.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HeaderDropdown = () => {
+	return (
+		<View>
+			<Text>HeaderDropdown</Text>
+		</View>
+	)
+}
+
+export default HeaderDropdown
+
+const styles = StyleSheet.create({})
