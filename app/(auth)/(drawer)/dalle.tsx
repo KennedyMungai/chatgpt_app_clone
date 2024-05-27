eeba3d4aa79b2dd2fadb661d862e4aa9773204db
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-27 12:23:55 +0300

    Set the title alignment to centre for the rest of the drawer pages

--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import HeaderDropdown from '@/components/header-dropdown'
+import { Stack } from 'expo-router'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const Dalle = () => {
+	const [gptVersion, setGptVersion] = useState('3.5')
+
 	return (
 		<View>
+			<Stack.Screen
+				options={{
+					headerTitle: () => (
+						<HeaderDropdown
+							title='ChatGPT'
+							items={[
+								{ key: '3.5', title: 'GPT-3.5', icon: 'bolt' },
+								{ key: '4.0', title: 'GPT-4', icon: 'sparkles' }
+							]}
+							selected={gptVersion}
+							onSelect={(key) => setGptVersion(key)}
+						/>
+					)
+				}}
+			/>
 			<Text>Dalle</Text>
 		</View>
 	)
