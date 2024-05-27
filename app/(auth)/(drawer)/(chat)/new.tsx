e4bd6cd8ea848938301034aba92fdde68e77b937
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-27 12:12:16 +0300

    Rendered the hader-dropdown component

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,12 +1,32 @@
+import HeaderDropdown from '@/components/header-dropdown'
+import { defaultStyles } from '@/constants/Styles'
 import { useAuth } from '@clerk/clerk-expo'
-import React from 'react'
+import { Stack } from 'expo-router'
+import React, { useState } from 'react'
 import { Button, StyleSheet, View } from 'react-native'
 
 const NewChatPage = () => {
+	const [gptVersion, setGptVersion] = useState<'3.5' | '4.0'>('3.5')
+
 	const { signOut } = useAuth()
 
 	return (
-		<View>
+		<View style={defaultStyles.pageContainer}>
+			<Stack.Screen
+				options={{
+					headerTitle: () => (
+						<HeaderDropdown
+							title='ChatGPT'
+							items={[
+								{ key: '3.5', title: 'GPT-3.5', icon: 'bolt' },
+								{ key: '4', title: 'GPT-4', icon: 'sparkles' }
+							]}
+							selected={gptVersion}
+							onSelect={(key) => console.log(key)}
+						/>
+					)
+				}}
+			/>
 			<Button title='Sign Out' onPress={() => signOut()} />
 		</View>
 	)
--- components/header-dropdown.tsx
+++ components/header-dropdown.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import * as DropdownMenu from 'zeego/dropdown-menu'
 
-const HeaderDropdown = () => {
+export type HeaderDropDownProps = {
+	title: string
+	selected: string
+	onSelect: (key: string) => void
+	items: {
+		key: string
+		title: string
+		icon: string
+	}[]
+}
+
+const HeaderDropdown = ({
+	title,
+	items,
+	onSelect,
+	selected
+}: HeaderDropDownProps) => {
 	return (
-		<View>
-			<Text>HeaderDropdown</Text>
-		</View>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<View>
+					<Text style={{ fontWeight: '500', fontSize: 16 }}>
+						{title}
+					</Text>
+				</View>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				{items.map((item) => (
+					<DropdownMenu.Item
+						key={item.key}
+						onSelect={() => onSelect(item.key)}
+					>
+						<DropdownMenu.ItemTitle>
+							{item.title}
+						</DropdownMenu.ItemTitle>
+						<DropdownMenu.ItemIcon androidIconName={item.icon} />
+					</DropdownMenu.Item>
+				))}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	)
 }
 
