db7b3a30891b8a57afc98f23bfaff31c8b212a60
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-28 14:53:51 +0300

    Added the markup and logic for the api key and organization

--- app/(auth)/(modal)/settings.tsx
+++ app/(auth)/(modal)/settings.tsx
@@ -1,14 +1,70 @@
+import Colors from '@/constants/Colors'
+import { Storage } from '@/utils/storage'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { useMMKVString } from 'react-native-mmkv'
 
 const SettingsPage = () => {
+	const [key, setKey] = useMMKVString('apiKey', Storage)
+	const [organization, setOrganization] = useMMKVString('org', Storage)
+
 	return (
-		<View>
-			<Text>Settings</Text>
+		<View style={styles.container}>
+			{key && key !== '' && (
+				<Text style={styles.label}>You are all set</Text>
+			)}
+
+			{(!key || key === '') && (
+				<>
+					<Text style={styles.label}>API Key & Organization</Text>
+					<TextInput
+						style={styles.input}
+						value={key}
+						onChangeText={setKey}
+						placeholder='Enter your API key'
+						autoCorrect={false}
+						autoCapitalize='none'
+					/>
+					<TextInput
+						style={styles.input}
+						value={organization}
+						onChangeText={setOrganization}
+						placeholder='Your organization'
+						autoCorrect={false}
+						autoCapitalize='none'
+					/>
+				</>
+			)}
 		</View>
 	)
 }
 
 export default SettingsPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 20,
+		paddingVertical: 20,
+		backgroundColor: 'white'
+	},
+	label: {
+		fontSize: 18,
+		marginBottom: 10
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: Colors.primary,
+		borderRadius: 5,
+		paddingHorizontal: 10,
+		paddingVertical: 10,
+		marginBottom: 20,
+		backgroundColor: '#fff'
+	},
+
+	buttonText: {
+		color: 'white',
+		textAlign: 'center',
+		fontSize: 16
+	}
+})
