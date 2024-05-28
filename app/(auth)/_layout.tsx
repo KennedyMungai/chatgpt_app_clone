e81670230e2a28171bdd0b04ac5c26bc7c3fab19
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-28 14:32:43 +0300

    Added some modal configs for the settings page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Stack } from 'expo-router'
+import { Stack, useNavigation } from 'expo-router'
+import { TouchableOpacity, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 const AuthLayout = () => {
+	const navigation = useNavigation()
+
 	return (
 		<Stack>
 			<Stack.Screen name='(drawer)' options={{ headerShown: false }} />
+			<Stack.Screen
+				name='(modal)/settings'
+				options={{
+					presentation: 'modal',
+					animation: 'slide_from_bottom',
+					animationDuration: 100,
+					headerShown: false,
+					headerTitle: 'Settings',
+					headerLeft: () => (
+						<TouchableOpacity onPress={() => navigation.goBack()}>
+							<Ionicons
+								name='close-outline'
+								size={24}
+								color='black'
+							/>
+						</TouchableOpacity>
+					)
+				}}
+			/>
 		</Stack>
 	)
 }
