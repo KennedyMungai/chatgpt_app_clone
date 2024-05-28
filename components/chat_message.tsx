770f282c6ceb4acb109fefcf05ec772266411b4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-28 10:31:47 +0300

    Created a chat message component

---
+++ components/chat_message.tsx
@@ -0,0 +1,15 @@
+import { Message } from '@/utils/interfaces'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ChatMessage = ({ content, role, imageUrl, prompt }: Message) => {
+	return (
+		<View>
+			<Text>ChatMessage</Text>
+		</View>
+	)
+}
+
+export default ChatMessage
+
+const styles = StyleSheet.create({})
